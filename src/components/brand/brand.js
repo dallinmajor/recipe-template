b09a3f9da5bb3b26743b78ef29087ee6ae00e551
Dally Wally <dallin.watac.major@gmail.com>
2019-01-30 12:29:50 -0700

    added some components

---
+++ src/components/brand/brand.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './brand.css';
+
+export default ({children})=> {
+    return (
+        <h1 className=''>Children</h1>
+    )
+}
--- src/components/link/link.js
+++ src/components/link/link.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './link.css';
 
 export default props => {
     return (
